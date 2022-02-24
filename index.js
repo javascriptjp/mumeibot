1826c90ff89373eaeb5648181aae8d0b4dff649b
Sosoかも <71214511+javascriptjp@users.noreply.github.com>
2022-02-24 19:37:47 +0900

    last reboot bug fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const {Worker} = require('worker_threads')
-const StartServer = (path)=>{Promise.all([new Promise(r=>new Worker(path).on('exit',r))]).then(()=>server(path))}
+const StartServer = (path)=>{Promise.all([new Promise(r=>new Worker(path).on('exit',r))]).then(()=>StartServer(path))}
 StartServer("./bot.main.js")
 const http = require('http');
 const server=http.createServer((n,t)=>{t.write("<3"),t.end()}).listen(8080)
