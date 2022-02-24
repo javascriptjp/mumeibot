6f98349fcccabd7de1705ffb6753eb22c8c7ed89
Sosoかも <71214511+javascriptjp@users.noreply.github.com>
2022-02-24 21:55:32 +0900

    Update get-regusers.js

--- bot_modules/control-command/get-regusers.js
+++ bot_modules/control-command/get-regusers.js
@@ -1,7 +1,58 @@
-module.exports = async(message,args) => {
+module.exports = async(message,args = []) => {
+    if(!args[0])return
+    if(!args[1])return
     const GettedMember = await message.guild.members.fetch()
-    const GettedReg = GettedMember.filter(i=>i.user.username=="Soso")
-    GettedReg.forEach(Get => {
-        message.channel.send(Get.user.username)
-    });
+    const NameReg = new RegExp(args[1],args[2]?args[2]:"u")
+    const GettedReg = GettedMember.filter(i=>NameReg.test(i.user.username))
+    if(args[0]=="kick"){
+        GettedReg.forEach(Get => {
+            const Gmember = message.guild.members.fetch(Get.user.id)
+            Gmember.kick()
+            message.channel.send({embeds: [{
+                color: "79bbff",
+                title: "Kicked member",
+                description: `<@${member.id}>は\`Kick\`されました`,
+                timestamp: new Date(),
+                footer: {
+                    icon_url: message.guild.iconURL(),
+                    text: "©️無名鯖 | setuna/Soso"
+                },
+            }]})
+        });
+        return;
+    }
+    if(args[0]=="ban"){
+        GettedReg.forEach(Get => {
+            const Gmember = message.guild.members.fetch(Get.user.id)
+            Gmember.ban()
+            message.channel.send({embeds: [{
+                color: "79bbff",
+                title: "Kicked member",
+                description: `<@${member.id}>は\`Kick\`されました`,
+                timestamp: new Date(),
+                footer: {
+                    icon_url: message.guild.iconURL(),
+                    text: "©️無名鯖 | setuna/Soso"
+                },
+            }]})
+        });
+        return;
+    }
+    if(args[0]=="get"){
+        GettedReg.forEach(Get => {
+            const Gmember = message.guild.members.fetch(Get.user.id)
+            Gmember.ban()
+            message.channel.send({embeds: [{
+                color: "79bbff",
+                title: "Get user info",
+                description: `<@${member.id}>`,
+                timestamp: new Date(),
+                footer: {
+                    icon_url: message.guild.iconURL(),
+                    text: "©️無名鯖 | setuna/Soso"
+                },
+            }]})
+        });
+        return;
+    }
 }
