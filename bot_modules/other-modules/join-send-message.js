6bc85567d358735cbccac982bd29ec2008655271
Sosoかも <71214511+javascriptjp@users.noreply.github.com>
2022-02-25 17:41:17 +0900

    Update join-send-message.js

--- bot_modules/other-modules/join-send-message.js
+++ bot_modules/other-modules/join-send-message.js
@@ -1,15 +1,17 @@
 const config = require("./join-send-message.json")
 module.exports = async (client,member) =>{
-    client.channels.cache.get(config.channel).send(`<@${member.id}>`)
-    client.channels.cache.get(config.channel).send({embeds: [{
-        color: "79bbff",
-        title: `<@${member.id}>さんようこそ無名鯖へ!`,
-        description: `<#945853334949093416>へ自己紹介をして参加ください!
-        また<#945838590674501642>でルールを確認してお楽しみください!`,
-        timestamp: new Date(),
-        footer: {
-            icon_url: member.guild.iconURL(),
-            text: "©️無名鯖 | setuna/Soso"
-        },
-    }]})
+    setTimeout(()=>{
+        client.channels.cache.get(config.channel).send(`<@${member.id}>`)
+        client.channels.cache.get(config.channel).send({embeds: [{
+            color: "79bbff",
+            title: `<@${member.id}>さんようこそ無名鯖へ!`,
+            description: `<#945853334949093416>へ自己紹介をして参加ください!
+            また<#945838590674501642>でルールを確認してお楽しみください!`,
+            timestamp: new Date(),
+            footer: {
+                icon_url: member.guild.iconURL(),
+                text: "©️無名鯖 | setuna/Soso"
+            },
+        }]})
+    },4000)
 }
