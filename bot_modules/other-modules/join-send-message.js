e13871a7c9a614d7dd094c6dfa4d7ac165c1f26b
Sosoかも <71214511+javascriptjp@users.noreply.github.com>
2022-02-25 17:44:11 +0900

    Update join-send-message.js

--- bot_modules/other-modules/join-send-message.js
+++ bot_modules/other-modules/join-send-message.js
@@ -4,7 +4,7 @@ module.exports = async (client,member) =>{
         client.channels.cache.get(config.channel).send(`<@${member.id}>`)
         client.channels.cache.get(config.channel).send({embeds: [{
             color: "79bbff",
-            title: `<@${member.id}>さんようこそ無名鯖へ!`,
+            title: `${member.user.username}さんようこそ無名鯖へ!`,
             description: `<#945853334949093416>へ自己紹介をして参加ください!
             また<#945838590674501642>でルールを確認してお楽しみください!`,
             timestamp: new Date(),
