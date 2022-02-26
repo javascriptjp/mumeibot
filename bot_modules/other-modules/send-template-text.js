fa60862e72a1b20b2fb03fc99abca0510ba12be2
Sosoかも <71214511+javascriptjp@users.noreply.github.com>
2022-02-26 17:15:20 +0900

    Update send-template-text.js

--- bot_modules/other-modules/send-template-text.js
+++ bot_modules/other-modules/send-template-text.js
@@ -1,20 +1,22 @@
 const config = require("./send-template-text.json")
 module.exports = async message => {
     if(message.channel.id==config.channel){
-        const beforeMessage = await message.channel.messages.fetch({ before: message.id, limit: 1 })
-            .then(messages => messages.first())
+        message.channel.messages.fetch({ before: message.id, limit: 1 })
+            .then(async messages => {
+                const beforeMessage = await messages.first()
+                if(beforeMessage.author.id!==config.BotId)return;
+                await beforeMessage.delete()
+                await message.channel.send({embeds: [{
+                    color: "79bbff",
+                    title: `__自己紹介テンプレート__`,
+                    description: `コピペしてお使いください\`\`\`\n名前:\n性別:\n年齢:\n趣味:\n一言:\n\`\`\``,
+                    timestamp: new Date(),
+                    footer: {
+                        icon_url: member.guild.iconURL(),
+                        text: "©️無名鯖 | setuna/Soso"
+                    },
+                }]})
+            })
             .catch(console.error)
-        if(beforeMessage.author.id!==config.BotId)return;
-        beforeMessage.delete()
-        message.channel.send({embeds: [{
-            color: "79bbff",
-            title: `__自己紹介テンプレート__`,
-            description: `コピペしてお使いください\`\`\`\n名前:\n性別:\n年齢:\n趣味:\n一言:\n\`\`\``,
-            timestamp: new Date(),
-            footer: {
-                icon_url: member.guild.iconURL(),
-                text: "©️無名鯖 | setuna/Soso"
-            },
-        }]})
     }
 }
