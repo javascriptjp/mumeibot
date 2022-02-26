08ef7b21a8346c41662d03666ffdd07be146922f
Sosoかも <71214511+javascriptjp@users.noreply.github.com>
2022-02-26 20:04:03 +0900

    update template

--- bot_modules/other-modules/send-template-text.js
+++ bot_modules/other-modules/send-template-text.js
@@ -1,6 +1,11 @@
+const { setTimeout } = require('timers/promises');
 const config = require("./send-template-text.json")
+
 module.exports = async message => {
     if(message.channel.id==config.channel){
+        if(!message.member.roles.cache.has(config.CheckRole)){
+            message.member.roles.add('690791160024924191')
+        }
         message.channel.messages.fetch({ before: message.id, limit: 1 })
             .then(async messages => {
                 const beforeMessage = await messages.first()
@@ -17,5 +22,21 @@ module.exports = async message => {
                 }]})
             })
             .catch(console.error)
+    }else{
+        if(message.member.roles.cache.has(config.CheckRole))return
+        const sendMessage = await message.channel.send({embeds: [{
+            color: "79bbff",
+            title: `__お知らせ__`,
+            description: `\nこのサーバーでは自己紹介をしてから参加していただいています。
+            <#945853334949093416>にて自己紹介をしてからご参加ください
+            \n__※自己紹介をしている状態でこのメッセージが出る場合__\n<@&680740561015144487>へメンションを付けてご連絡ください\n`,
+            timestamp: new Date(),
+            footer: {
+                icon_url: message.guild.iconURL(),
+                text: "©️無名鯖 | setuna/Soso"
+            },
+        }],content:`<@${message.author.id}>`})
+        await setTimeout(10000);
+        await sendMessage.delete();
     }
 }
