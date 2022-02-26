b8984bdb36a7904e95bb9a739911d5db99bf6385
Sosoかも <71214511+javascriptjp@users.noreply.github.com>
2022-02-26 17:20:27 +0900

    くっそ単純で間違えようのないバグの修正

--- bot_modules/other-modules/react-to-text-fix.js
+++ bot_modules/other-modules/react-to-text-fix.js
@@ -1,7 +1,7 @@
 const config = require("./react-to-text-fix.json")
 module.exports = message => {
     if(message.channel.id==config.channel){
-        if(message.author.id!==config.BotId)return;
+        if(message.author.id==config.BotId)return;
         setTimeout(()=>{
             message.react("👍")
         },1000)
