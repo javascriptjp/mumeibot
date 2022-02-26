81a2b5bd0d06fa22d9ed4ab1f0641cd09c2a6a4f
Sosoかも <71214511+javascriptjp@users.noreply.github.com>
2022-02-26 17:33:18 +0900

    Update send-template-text.js

--- bot_modules/other-modules/send-template-text.js
+++ bot_modules/other-modules/send-template-text.js
@@ -8,7 +8,7 @@ module.exports = async message => {
                 await message.channel.send({embeds: [{
                     color: "79bbff",
                     title: `__自己紹介テンプレート__`,
-                    description: `コピペしてお使いください\`\`\`\n名前:\n性別:\n年齢:\n趣味:\n一言:\n\`\`\``,
+                    description: `コピペしてお使いください\`\`\`\n名前:\n性別:\n年齢:\n趣味:\n一言:\n\`\`\`\n※こちらはメッセージと同時に送信されます`,
                     timestamp: new Date(),
                     footer: {
                         icon_url: message.guild.iconURL(),
